09c8436363fa3fede5264a251ac180634e2c7f8f
TJ <46174306+ttgtom@users.noreply.github.com>
2018-12-26 21:45:54 -0500

    Create contribute.sql
    If you're going to contribute to our repository - we'll grant you usage of our network of searching records. 
    Register as a contributor on our site to become a resource. 
    *Disclaimer DO NOT STORE Any external information - OR change the table structure. These specifications are crucial to functioning. *

---
+++ contribute.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 27, 2018 at 03:42 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Repository`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fraud_records`
+--
+
+CREATE TABLE `fraud_records` (
+  `id` int(225) NOT NULL,
+  `username` text NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `reason` text NOT NULL,
+  `screenshots` text NOT NULL,
+  `ip` varchar(70) NOT NULL,
+  `type` int(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
