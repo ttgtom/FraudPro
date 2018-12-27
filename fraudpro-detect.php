f11f75f37e0e9db29db8421b70dc8ab6e44c805f
TJ <46174306+ttgtom@users.noreply.github.com>
2018-12-26 21:37:44 -0500

    Create fraudpro-detect.php
    This file is needed if you are going to utilize the community records of  fraud. 
    Place this file in your included folder & include the document where you want to check users, at registration or visitation.

---
+++ fraudpro-detect.php
@@ -0,0 +1,24 @@
+ <?php
+include 'conf.php';
+//We "Import/Include" this so we can access our credentials.
+    $key = "ENTERKEY"; //Register to get a key on www.FraudPro.org/Community
+    $searchterm = $_GET['searchterm']; //Types: email, ip, phone+
+   $site = "www.example.com";
+   //include full domain(ex. example.com)
+$source = "https://fraudpro.org/api/detect.php"
+//If you plan to use our system, leave this.
+//If the information entered, is in our database, we will return a boolean response. 
+   if ($source = "true"){
+return true;
+echo 'true';
+//You can add functions under this section. Example: Exit the session
+exit("Detection of fraud.");
+}
+else {
+return false
+echo 'false';
+//You can enable loading operations if the result = false. OR leave this empty
+
+}
+   
+?>
